4370a3498b07fa577559a882671f53801f7063a5
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 15:31:33 +0700

    rest param

--- restParam.js
+++ restParam.js
@@ -1,7 +1,18 @@
-function sum(...rest) {
+// function sum(...rest) {
+//   let sum = 0;
+//   for (let number of rest) sum += number;
+//   return sum;
+// }
+// let x = sum(4, 9, 16, 25, 29, 100, 66, 77); // <-- output "326"
+
+
+function sum(initVal, ...rest) {
   let sum = 0;
   for (let number of rest) sum += number;
-  return sum;
+  return initVal + sum;
 }
-let x = sum(4, 9, 16, 25, 29, 100, 66, 77); // <-- output "326"
+
+let x = sum(10, 5); // <-- output "15"
+
+
 console.log(x);
