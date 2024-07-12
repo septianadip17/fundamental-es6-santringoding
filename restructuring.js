31c840a104c2477b2cc91b3d2b76797a7ec36aa8
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 15:34:05 +0700

    restructuring

---
+++ restructuring.js
@@ -0,0 +1,9 @@
+//define variable
+const name = "Fika Ridaul Maulayya";
+const age = 25;
+
+//restructuring
+const obj = { name, age };
+
+//print output
+console.log(obj); // <-- {name: 'Fika Ridaul Maulayya', age: 25}
