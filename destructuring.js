7b9b067adb9ce6a9c3c12366b0e1f32689c6d424
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 15:33:18 +0700

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,23 @@
+//array
+const arr = ["Fika", "Ahmad", "Lutfi"];
+
+//destructuring
+const [name1, name2, name3] = arr;
+
+//print output
+console.log(name1); // <-- "Fika"
+console.log(name3); // <-- "Ahmad"
+console.log(name3); // <-- "Lutfi"
+
+//object
+const obj = {
+  name: "Fika Ridaul Maulayya",
+  age: 25,
+};
+
+//destructuring
+const { name, age } = obj;
+
+//print ouput
+console.log(name); // <-- "Fika Ridaul Maulayya"
+console.log(age); // <-- "25"
