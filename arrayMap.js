7c7e2372178683ce44f5941353025a5461b33320
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-12 15:38:21 +0700

    array map

---
+++ arrayMap.js
@@ -0,0 +1,10 @@
+//define array
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+//looping
+const number = arr.map( value => {
+  return value;
+});
+
+//print output
+console.log(number); // [1, 2, 3, 4, 5, 6, 7]
